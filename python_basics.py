a88775164d553c1684a838d757c45db2c509644a
joaoppcamorim <joaoppcamorim@gmail.com>
2023-03-20 19:31:25 +0000

    first commit

---
+++ python_basics.py
@@ -0,0 +1,70 @@
+# Variables
+# assignement
+ex_var = 5
+
+# reassignment
+ex_var = 7
+
+# Basic Data Types
+# Floats
+float_1 = 1.2345
+float_2 = -1.25
+float_3 = 0.0
+float_4 = 13.0
+float_5 = -0.5
+
+# Integers
+int_1 = 7
+int_2 = -52
+int_3 = 0
+int_4 = 900
+int_5 = -749
+
+# Booleans
+bool_1 = True
+boll_2 = False
+
+# Comments and Math Operators
+# single line
+
+# Math Operators
+addition = 4 + 5 # 9
+subtraction = 5 - 2 # 3
+division = 7 / 2 # 3.5
+multiplication = 3 * 8 # 24
+exponentiation = 4 ** 4 # 256
+floor_division = 16 // 5 # 3
+modulo = 7 % 3 # 1
+
+# Assignment operators
+add_assign = 5
+add_assign = add_assign + 7 # 12
+add_assign += 7 # 12
+
+# print()
+
+example_1 = 3.14159
+example_2 = 24601
+example_3 = True
+example_4 = 10 + 9
+
+print(example_1)
+print(example_2)
+print(example_3)
+print(2019)
+print(example_4)
+print((4+5)/3)
+
+# Float Approximation Errors
+print(1.23 + 2.80)
+
+# Getting arround
+# Avoid using floats
+
+example_5 = (123 + 280) / 100
+print(example_5)
+
+# using round()
+
+example_6 = round(1.23 + 2.80, 2)
+print(example_6)
